3465840c21f03279b5abd3f0bbf3045a6fd43e3e
pablomigo <pablo_migo@yahoo.com>
2017-11-07 22:13:22 -0600

    Update

--- src/groupStruct/GroupEdit.java
+++ src/groupStruct/GroupEdit.java
@@ -1,5 +1,10 @@
 package groupStruct;
 
 public class GroupEdit {
+	
+	public void editGroup(Group group, String groupName, String fileName, String className,
+			String location, String locationNotes, String time, String access, String seatLimit){
+		
+	}
 
 }
