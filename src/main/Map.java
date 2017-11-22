46fc429607ad8ae3260b367e7910a25ccdb76f01
Carlscardenas <Carla.m.cardenas.m@gmail.com>
2017-11-21 18:35:42 -0600

    working map this time

---
+++ src/main/Map.java
@@ -0,0 +1,9 @@
+package main;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+public class Map {
+
+}
