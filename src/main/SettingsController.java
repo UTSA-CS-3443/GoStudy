97572ae94ee68b4f247f325d4ce5765aad154e22
gloriamarissatrevino <gloriamarissatrevino@172.24.49.4>
2017-11-21 20:58:08 -0600

    added Change Password fucntion- Victoria and Gloria

--- src/main/SettingsController.java
+++ src/main/SettingsController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import users.User;
 import javafx.scene.control.Alert.AlertType;
 
 public class SettingsController extends CommonController{
@@ -26,10 +27,18 @@ public class SettingsController extends CommonController{
 
     @FXML
     void changePasswordButtonPressed(ActionEvent event) {
-    	Alert confirm = new Alert(AlertType.CONFIRMATION);
-    	confirm.setHeaderText("You cant change your password >:(");
-    	confirm.setTitle("User Confirmation");
-    	confirm.show();
+    		String newPassword = passwordTextField.getText();
+    		String Username = "annie";
+    		User user = new User();
+    		int boo = user.changePassword(newPassword, Username);
+    		System.out.println(boo);
+    		if(boo == 1){
+    	
+    			Alert confirm = new Alert(AlertType.CONFIRMATION);
+    			confirm.setHeaderText("You cant change your password >:(");
+    			confirm.setTitle("User Confirmation");
+    			confirm.show();
+    		}
     }
 
     @FXML
